db5bee72ea2cae122bc7d3e10632dc5f3720d045
Ворончихин Иван <aqua.dev@yandex.com>
2026-04-01 18:17:02 +0300

    Init config server

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.5"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.aquadev"
+version = "0.0.1-SNAPSHOT"
+description = "config-server"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.1.1"
+val springBootAdminVersion by extra("4.0.2")
+val opentelemetryVersion by extra("2.21.0-alpha")
+
+dependencies {
+    implementation("de.codecentric:spring-boot-admin-starter-client")
+    implementation("org.springframework.cloud:spring-cloud-config-server")
+    implementation("org.springframework.cloud:spring-cloud-starter-kubernetes-discoveryclient")
+    implementation("org.springframework.boot:spring-boot-starter-opentelemetry")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.opentelemetry.instrumentation:opentelemetry-logback-appender-1.0:${opentelemetryVersion}")
+    testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+        mavenBom("de.codecentric:spring-boot-admin-dependencies:$springBootAdminVersion")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/aquadev/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.aquadev.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
